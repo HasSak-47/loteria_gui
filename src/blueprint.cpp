a9a2aaaa421a156e2d0688ff06d23d90582072df
Ulises Alanis <ulises.alanis.255@gmail.com>
2024-04-12 21:55:31 -0600

    gui

--- src/blueprint.cpp
+++ src/blueprint.cpp
@@ -11,40 +11,54 @@ ButtonType operator^(ButtonType a, ButtonType b) { return ENUM_OPER(^); }
 BlueprintCell::BlueprintCell(size_t x, size_t y, Blueprint* parent) : QWidget(parent) {
 	this->x = x;
 	this->y = y;
+	this->_blueprint = parent;
+	this->_child = nullptr;
+}
 
-	this->_random = new QPushButton("Random", this);
-	this->_copy = new QPushButton("SetTo", this);
-	this->_setto = new ButtonLine(this);
+void BlueprintCell::create_button(const char* text){
+	this->_child = new QPushButton(text, this);
+	this->_blueprint->_grid->addWidget(this->_child, x, y);
+	connect((QPushButton*)this->_child, &QPushButton::clicked, this, &BlueprintCell::toggle_type);
+}
 
-	((QGridLayout*)this->parent())->addWidget(this->_random, x, y);
-	((QGridLayout*)this->parent())->addWidget(this->_copy, x, y);
-	((QGridLayout*)this->parent())->addWidget(this->_setto, x, y);
+LineButton::LineButton(QWidget* parent) : QWidget(parent) {
+	button = new QPushButton("c", this);
+	line = new QLineEdit(this);
 
-	this->render();
+	button->setFixedWidth(20);
+
+	layout = new QGridLayout(this);
+	layout->addWidget(line, 0, 0);
+	layout->addWidget(button, 0, 1);
+	setLayout(layout);
+}
+
+LineButton::~LineButton() {
+	delete button;
+	delete line;
 }
 
 void BlueprintCell::render(){
-	this->setLayout(this->_setto->layout);
+	if (this->_child != nullptr){
+		_blueprint->_grid->removeWidget(this->_child);
+		delete this->_child;
+	}
+
 	switch (this->_type & ButtonType::TYPE_MASK){
 		case ButtonType::Random:
-			this->_setto->hide();
-			this->_random->show();
-			this->_copy->hide();
+			this->create_button("Random");
 			break;
 		case ButtonType::MarkCopy:
-			this->_setto->hide();
-			this->_random->hide();
-			this->_copy->show();
+			this->create_button("MarkCopy");
 			break;
 		case ButtonType::SetTo:
-			this->_setto->show();
-			this->_random->hide();
-			this->_copy->hide();
+			this->_child = new LineButton(this);
+			_blueprint->_grid->addWidget(this->_child, x, y);
+			connect(((LineButton*)this->_child)->button, &QPushButton::clicked, this, &BlueprintCell::toggle_type);
 			break;
 		default:
 			break;
 	}
-
 }
 
 void BlueprintCell::toggle_type(){
@@ -57,10 +71,11 @@ void BlueprintCell::toggle_type(){
 			break;
 		case ButtonType::SetTo:
 			this->_type = ButtonType::Random;
-		break;
+			break;
 		default:
 			break;
 	}
+
 	this->render();
 }
 
@@ -74,7 +89,42 @@ Blueprint::Blueprint(QWidget* parent) : QWidget(parent) {
 	}
 
 	setLayout(this->_grid);
-
 }
 
+const std::string options[] = {
+	"Random",
+	"MarkCopy",
+	"SetTo",
 
+	"SetCount",
+};
+
+std::vector<BlueprintParam> Blueprint::build(){
+	std::vector<BlueprintParam> params = {};
+
+	for (size_t x = 0; x < 4; x++){
+		for (size_t y = 0; y < 4; y++){
+			auto* cell = this->_cells[x][y];
+			switch(cell->_type & ButtonType::TYPE_MASK){
+				case ButtonType::Random:
+					break;
+				case ButtonType::MarkCopy:
+					params.push_back({"MarkCopy", {x, y}});
+					break;
+				case ButtonType::SetTo:
+					params.push_back({"SetTo", {
+						x,
+						y,
+						((LineButton*)cell->_child)->line->text().toUInt()
+					}});
+					break;
+				default:
+					break;
+			}
+		}
+	}
+
+	params.push_back({"SetCount", {4}});
+
+	return params;
+}
